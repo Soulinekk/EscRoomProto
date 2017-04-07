2cf342d74fbf810f6e489349852c1e3aef48ef02
Marek Chodan <marcjo04@wp.pl>
2017-04-07 15:23:07 +0200

    update: kolejne elementy dodane (przewidywane ukonczenie prototypu poniedziałek)

---
+++ Assets/Scripts/LvLTwo/InteractivElements/Lamp.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lamp : InteractivElement {
+    protected override void Start()
+    {
+        base.Start();
+        actualState = States.Closed;
+    }
+
+    protected override void FixedUpdate()
+    {
+        if (actionClickCount == 5)
+        {
+            SequenceOn = false;
+            actualState = States.Broken;
+            actionClickCount = 0;
+
+        }
+        base.FixedUpdate();
+    }
+
+    protected override IEnumerator OnClickAction()
+    {
+        switch (actualState)
+        {
+            case States.Closed:
+                //Debug.Log(Inventory.Instance.activeElement.objName);
+                if (Inventory.Instance.activeElement.objName == "bulb")
+                {
+                    Feedback.Instance.ShowText("Doesn't fit well, but it works", 1.5f);
+                    actualState = States.Open;
+                    SequenceOn = true;
+                }
+                else
+                {
+                    Feedback.Instance.ShowText("That's a lamp", 1f);
+                }
+                break;
+            case States.Open:
+
+                Feedback.Instance.ShowText("Looks fine", 0.7f);
+                break;
+
+            case States.Broken:
+                Feedback.Instance.ShowText("meh... still Looks fine", 0.7f);
+
+                break;
+            /*
+            case States.PhaseTwo:
+                
+                break;
+            
+    case States.PhaseFour:
+        break;
+        */
+            default:
+                break;
+
+        }
+        return base.OnClickAction();
+    }
+}
